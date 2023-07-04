01f5526dab6671c9d6d294705a565c3b981a1939
H5Ryder <ryderharris@icloud.com>
2023-07-04 19:48:49 +0100

    Added pdf describing Version 1 logic

--- software/version1/version1.ino
+++ software/version1/version1.ino
@@ -1,13 +1,17 @@
 //BIG LIST OF CONSTANTS
 #define yHomeSteps 700 //STEPS TO TAKE Y FROM MICROSWITCH (ACTIVATED) POSITION TO 0 (So that the gun is horizontal)
 
-#define canvasHeight 2000
-#define canvasWidth 500
-#define gunOffset 1660
-#define stepsPerRev 800 //X and Y Stepper Steps
+#define canvasHeight 1800
+#define canvasWidth 1800
+#define gunOffset 2300
+#define stepsPerRev800 //X and Y Stepper Steps
 #define stepsPerRevTrigger 400 //Trigger Stepper Steps
 #define gearRatio 8 //Ratio between pulleys
 #define degreeToSteps 17.78 //For every one degree angle change, the stepper moves 17.78 steps
+
+#define m 20;
+
+
 
 //-----------------------------------------------------------
 #include <AccelStepper.h>
@@ -35,6 +39,9 @@ AccelStepper stepperX = AccelStepper(motorInterfaceTypeX, stepPinX, dirPinX);
 
 
 void setup() {
+  int t = 500;
+  bool fire = true;
+  Serial.begin(9600);
   pinMode(limitY, INPUT);  //Set limit switch
 
   stepperY.setMaxSpeed(1000);
@@ -44,16 +51,76 @@ void setup() {
   stepperT.setMaxSpeed(600);
   stepperT.setAcceleration(600);
 
-  while (digitalRead(limitY)){
+  
 
-  }
+  while (digitalRead(limitY)) { 
 
-  homeY();
-  //homeX(-10);
+    }
+
+homeY();
+delay(100);
+
+    
+
+  
   //homeTrigger();
   //testTrigger();
-  delay(1000);
-  aimAndFire(10, 100, true);
+ // delay(1000);
+  //aimAndFire(10, 100, false);
+
+  
+
+    for (int i = 0; i < 51; i++) {
+   int xCoord[51] = {6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 15, 15, 15, 14, 13, 12, 11, 10, 10, 10, 10, 10};
+    int yCoord[51] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 32, 32, 32, 32, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 20, 19, 18, 17, 16};
+    int xVal = xCoord[i]*m;
+    int yVal = yCoord[i]*m;
+    Serial.println(xVal);
+    aimAndFire(xVal, yVal, fire);
+    delay(t);
+}
+
+  for (int i = 0; i < 51; i++) {
+   int xCoord[51] = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 8, 7, 9, 9, 9, 9, 9, 10, 11, 12, 11, 10, 19, 20, 21, 22, 23, 24, 25, 26, 27, 27, 27, 27, 26, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19};
+    int yCoord[51] = {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 4, 4, 4, 24, 25, 26, 27, 28, 28, 27, 26, 25, 24, 32, 32, 32, 32, 32, 32, 32, 32, 32, 31, 30, 29, 29, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19};
+    int xVal = xCoord[i]*m;
+    int yVal = yCoord[i]*m;
+    Serial.println(xVal);
+    aimAndFire(xVal, yVal, fire);
+    delay(t);
+}
+
+for (int i = 0; i < 51; i++) {
+   int xCoord[51] = {19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 25, 24, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 24, 25, 26, 27, 27, 27, 27, 26, 25, 24, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 24, 25, 26};
+    int yCoord[51] = {18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 29, 29, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 20, 20, 20, 20, 19, 18, 17, 17, 17, 17, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 7, 7, 7};
+      int xVal = xCoord[i]*m;
+    int yVal = yCoord[i]*m;
+    Serial.println(xVal);
+    aimAndFire(xVal, yVal, fire);
+    delay(t);
+}
+for (int i = 0; i < 51; i++) {
+   int xCoord[51] = {27, 27, 27, 27, 26, 25, 24, 23, 22, 21, 20, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 32, 33, 34, 35, 36, 36, 36, 36, 37, 38, 39, 40, 40, 41, 40, 41, 42, 43, 44};
+    int yCoord[51] = {7, 6, 5, 4, 4, 4, 4, 4, 4, 4, 4, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 4, 4, 4, 4, 5, 6, 6, 7, 8, 9, 8};
+      int xVal = xCoord[i]*m;
+    int yVal = yCoord[i]*m;
+    Serial.println(xVal);
+    aimAndFire(xVal, yVal, fire);
+    delay(t);
+}
+
+for (int i = 0; i < 51; i++) {
+   int xCoord[51] = {45, 46, 47, 47, 47, 45, 39, 39, 48, 48, 48, 49, 50, 51, 51, 51, 51, 52, 53, 54, 55, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 43, 44, 42, 41, 45, 45, 45, 45, 45};
+    int yCoord[51] = {7, 6, 6, 5, 4, 6, 6, 5, 6, 5, 4, 4, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 32, 32, 32, 32, 32, 31, 30, 29, 28};
+      int xVal = xCoord[i]*m;
+    int yVal = yCoord[i]*m;
+    Serial.println(xVal);
+    aimAndFire(xVal, yVal, fire);
+    delay(t);
+}
+
+
+
 
 }
 
@@ -66,7 +133,7 @@ void loop() {
 void homeY() {
 
   stepperY.setCurrentPosition(0);
-  stepperY.moveTo(-900);
+  stepperY.moveTo(-50);
   stepperY.runToPosition();
 
   while (digitalRead(limitY)) {  // Make the Stepper move CCW until the switch is activated
@@ -119,6 +186,8 @@ void aimAndFire(int x, int y, bool fire) {
   rotateY(calculateYAngleSteps(y));
   rotateX(calculateXAngleSteps(x));
 
+  Serial.println("working2");
+
   if (fire) {pullTrigger();}
 
 
@@ -159,3 +228,6 @@ void pullTrigger() {
   stepperT.runToPosition();
 
 }
+
+
+
